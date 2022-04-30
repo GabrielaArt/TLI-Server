8b53814968c24d21a546bac4383f10f99d6388db
EduHerr <eduherrald@gmail.com>
2022-04-30 18:31:41 -0500

    Push

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const { VueLoaderPlugin } = require('vue-loader');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/app/index.js',
+    output: {
+        path: __dirname + '/src/public/js',
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use:{ 
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            },
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [ { loader: 'file-loader'} ],
+            }
+        ]
+    },
+    plugins: [
+        new VueLoaderPlugin()
+    ]
+};
